2b13e958b183839e8efb06de2a1a31857be62542
Preacherman194589 <delonjackson@yahoo.com>
2022-09-01 16:52:43 -0700

    intisl commit

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -33,7 +33,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto neighborhoods GeoJSON URL..
-let torontoHoods = "https://raw.githubusercontent.com/Preacherman194589/Mapping_Earthquakes/main/torontoNeighborhoods(1).json";
+let torontoHoods = "https://raw.githubusercontent.com/Preacherman194589/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/Preacherman194589/Mapping_Earthquakes/main/torontoRoutes.json";
