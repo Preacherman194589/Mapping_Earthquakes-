ffc9c03992ffce9dc064614c3b697bf860f8d2aa
Preacherman194589 <delonjackson@yahoo.com>
2022-09-01 17:52:05 -0700

    intial commit

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,123 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        accessToken: API_KEY
+    });
+
+ // We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: API_KEY
+  });
+    
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets  
+};
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid', {
+  center: [39.5, -98.5], 
+  zoom: 3,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL..
+let torontoHoods = "https://raw.githubusercontent.com/Preacherman194589/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/Preacherman194589/Mapping_Earthquakes/main/torontoRoutes.json";
+  
+    // Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Preacherman194589/Mapping_Earthquakes/main/majorAirports.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  //console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+
+// We turn each feature into a circleMarker on the map.
+    
+pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo 
+       
+    }).addTo(map);
+});
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+ // This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+  
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+   // [33.9416, -118.4085],
+   // [37.6213, -122.3790],
+   // [40.7899, -111.9791],
+    //[47.4502, -122.3088]
+  ];
+ 
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+   // color: "yellow"
+  }).addTo(map);
+
+ // Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+
+  L.circleMarker(city.location, {
+   // color: 'red',
+    //lineweight: 4.0,
+   //radius: city.population/100000
+  })
+  //.bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population" + city.population.toLocaleString() + "</h3>")
+  //.addTo(map);
+});
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+ 
